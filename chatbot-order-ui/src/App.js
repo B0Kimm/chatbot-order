9af0022d5c5c3b3dd98da5ccfbcf542aa81bafda
Kimm <bo.kim0226@gmail.com>
2020-10-11 22:07:57 +0900

    111020

--- chatbot-order-ui/src/App.js
+++ chatbot-order-ui/src/App.js
@@ -2,15 +2,23 @@ import React from 'react';
 import { Provider } from 'react-redux'
 //import TodoInput from './compoenets/Todoinput'
 //import TodoList from './compoenets/TodoList'
-import store from './store'
-import Signup from './compoenets/Signup'
+import store from './store/index.js'
+import AddressForm from './compoenets/AddressForm'
+import Checkout from './compoenets/Checkout'
+import PaymentForm from './compoenets/PaymentForm'
+import OrderReview from './compoenets/OrderReview'
+import Basket from './compoenets/Basket'
 
 
 const App = () => {
   return <>
   <Provider store = {store}>
   <div style = {{width : "1000px", margin: "0 auto"}}>
-      <Signup/>
+      <Basket/>
+      <AddressForm/>
+      <Checkout/>
+      <PaymentForm/>
+      <OrderReview/>
     </div></Provider>
     </>
 }
